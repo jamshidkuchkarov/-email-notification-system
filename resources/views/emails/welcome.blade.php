4b1ed981bc29846d47ea0fa1b171c80d47b0fbcc
jamshid <jamshidqochqorov84@gmail.com>
2025-03-01 17:13:57 +0500

    Laravel queue

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<h1>Salom, {{ $user->name }}!</h1>
+<p>Xush kelibsiz! Siz bizning tizimga muvaffaqiyatli ro‘yxatdan o‘tdingiz.</p>
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Jobs\SendEmailJob;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', function (Request $request) {
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    SendEmailJob::dispatch($user);
+
+    return response()->json(['message' => 'User registered, email will be sent']);
+});
